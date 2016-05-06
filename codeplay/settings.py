836f2254e6cbdb42d55c61c53b53c8368c4ade8b
AMiT Kumar <dtu.amit@gmail.com>
2016-05-06 20:59:34 +0530

    Add static files

--- codeplay/settings.py
+++ codeplay/settings.py
@@ -120,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), './..'))
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
